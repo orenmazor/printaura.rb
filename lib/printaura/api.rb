aa557e4c2379a4a97f0634da6568e20f01d5a824
oren mazor <oren.mazor@gmail.com>
2015-02-11 13:53:04 -0500

    basic api

--- lib/printaura/api.rb
+++ lib/printaura/api.rb
@@ -3,8 +3,13 @@ require 'httparty'
 module Printaura
   class Api
     def self.method_missing(m, *args, &block)
-      body = {key: Printaura::Config.API_KEY, hash: Printaura::Config.API_HASH, method: m}.map {|k,v| "#{k}=#{v}"}.join("&")
-      result = JSON.parse(HTTParty.post("http://www.api.printaura.com/api.php", {body: body}).to_s)
+      #we only really care about the array options
+      options = args.last || {}
+
+      postparams= options.update({key: Printaura::Config.API_KEY, hash: Printaura::Config.API_HASH, method: m}).map {|k,v| "#{k}=#{v}"}.join("&")
+
+      puts postparams
+      result = JSON.parse(HTTParty.post("http://www.api.printaura.com/api.php", body: postparams))
       raise result["message"] unless result["status"]
 
       return result
--- lib/printaura/product.rb
+++ lib/printaura/product.rb
@@ -3,7 +3,15 @@ require "printaura/api"
 module Printaura
   class Product
     def self.all
-      api.get(method: "viewproducts")
+      # do i care?
+      api.viewproducts.each do |prod|
+        puts prod
+      end
+    end
+
+    def initialize(brand, colors, tags, image_path)
+      image_id = api.uploadimage(image_path)
+      api.addproduct(args)
     end
   end
 end
