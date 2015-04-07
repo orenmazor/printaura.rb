105fdbe883f156a1c1f0202b516a70d380cc419b
oren mazor <oren.mazor@gmail.com>
2015-04-07 11:54:54 -0400

    track the product ids we just created

--- lib/printaura/product.rb
+++ lib/printaura/product.rb
@@ -2,6 +2,9 @@ require "printaura/api"
 
 module Printaura
   class Product
+    attr_accessor :shopify_product_id
+    attr_accessor :printaura_product_id
+
     def self.all
       # do i care?
       Api.viewproducts.each do |prod|
@@ -9,13 +12,15 @@ module Printaura
       end
     end
 
-    def initialize(brand, colors, image_id, price, app_ids, title, product_id, sku, shopify)
+    def initialize(brand, colors, image_id, mock_image_id, price, app_ids, title, product_id, sku, shopify)
       # lol :(
-      args = {brand_id: brand, colors: Base64.encode64(colors.to_json), front_print: image_id, title: title, description: "", product_price: price, apps: Base64.encode64(app_ids.to_json), product_id: product_id, sku: sku, storeimages: Base64.encode64({"0"=>{"image_id"=> image_id, "default"=>"1"}}.to_json)}
+      store_images = {"0" => {"image_id" => image_id, "default" => "0"}, "1" => {"image_id" => mock_image_id, "default"=>"1"}}
+      args = {brand_id: brand, colors: Base64.encode64(colors.to_json), front_print: image_id, front_mockup: mock_image_id, title: title, description: "", product_price: price, apps: Base64.encode64(app_ids.to_json), product_id: product_id, sku: sku, storeimages: Base64.encode64(store_images.to_json)}
       if shopify
         args = args.merge(shopify)
       end
-      Api.addproduct(args)
+      @shopify_product_id = Api.addproduct(args)["results"]["shopify_id"]
+      @printaura_product_id = Api.addproduct(args)["results"]["product_id"]
     end
   end
 end
