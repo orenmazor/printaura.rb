002a74c4424aece62693547f839f0713aea82e51
oren mazor <oren.mazor@gmail.com>
2015-04-08 06:45:17 -0400

    stupid bug

--- lib/printaura/product.rb
+++ lib/printaura/product.rb
@@ -6,21 +6,21 @@ module Printaura
     attr_accessor :printaura_product_id
 
     def self.all
-      # do i care?
       Api.viewproducts.each do |prod|
         puts prod
       end
     end
 
     def initialize(brand, colors, image_id, mock_image_id, price, app_ids, title, product_id, sku, shopify)
-      # lol :(
       store_images = {"0" => {"image_id" => image_id, "default" => "0"}, "1" => {"image_id" => mock_image_id, "default"=>"1"}}
       args = {brand_id: brand, colors: Base64.encode64(colors.to_json), front_print: image_id, front_mockup: mock_image_id, title: title, description: "", product_price: price, apps: Base64.encode64(app_ids.to_json), product_id: product_id, sku: sku, storeimages: Base64.encode64(store_images.to_json)}
       if shopify
         args = args.merge(shopify)
       end
-      @shopify_product_id = Api.addproduct(args)["results"]["shopify_id"]
-      @printaura_product_id = Api.addproduct(args)["results"]["product_id"]
+
+      result = Api.addproduct(args)
+      @shopify_product_id = result["results"]["shopify_id"]
+      @printaura_product_id = result["results"]["product_id"]
     end
   end
 end
